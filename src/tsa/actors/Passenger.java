8bddf9e295a3099bcf051b27589413941bdb0ca7
Brandon Cole <bnc8088@rit.edu>
2013-01-28 16:40:58 -0500

    Initial test commit (Passenger.java)

---
+++ src/tsa/actors/Passenger.java
@@ -0,0 +1,12 @@
+package tsa.actors;
+
+import akka.actor.UntypedActor;
+
+
+public class Passenger extends UntypedActor {
+	@Override
+	public void onReceive(Object arg0) throws Exception {
+		
+	}
+	
+}
